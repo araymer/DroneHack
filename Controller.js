9e7fe4489fc81229549e4801a6aac30a91c70218
Ethan <emanns95@gmail.com>
2016-01-23 16:07:23 -0700

    Added aunotomy and manual path making.

--- Controller.js
+++ Controller.js
@@ -7,6 +7,16 @@ var decision = require('./Flight/decision');
 
 var data;
 
+/*
+Test of movements using ar-drone api
+*/
+decision.lightShow(client);
+
+decision.takeoff(client);
+decision.turnLeft(client, .2, 1000);
+decision.moveForward(client, .2, 5000);
+decision.land(client);
+
 /**
 * @param client the client to the drone
 * Simple call for executing whatever the current decision is.
@@ -25,10 +35,12 @@ function getData(client) {
 	return decision.data(client);
 }
 
+//data = decision.data(client);
+//console.log(data);
 
-data = decision.data(client);
-console.log(data);
 
 
-var testLed = decision.lightShow(client);
-var testNav = decision.navdata(client);
+// test(client);
+//var testLed = decision.lightShow(client);
+//var testData = decision.data(client);
+//var testSquare = decision.testSquare(client);
