3cf1b05adaf3ceeffec80d1ca24049b89959a802
Ethan <emanns95@gmail.com>
2016-01-23 09:43:50 -0700

    Added navData to decision for getting navigation data and drone state.

--- Controller.js
+++ Controller.js
@@ -2,9 +2,15 @@
 
 var arDrone = require('ar-drone');
 var decision = require('./Flight/decision');
+<<<<<<< Updated upstream
 
 var client = arDrone.createClient();
 var data;
+=======
+//var client = require('./client');
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+>>>>>>> Stashed changes
 
 /**
 * @param client the client to the drone
@@ -24,10 +30,15 @@ function getData(client) {
 	return decision.data(client);
 }
 
-
 var dataLoop = setInterval(function() {
-	data = decision.data(client);	
+	data = decision.data(client);
 	console.log(data);
 }, 33);
 
+// var test = decision.data();
 
+// test(client);
+var testLed = decision.lightShow(client);
+var testNav = decision.navdata(client);
+//var test = decision.takeoff(client);
+>>>>>>> Stashed changes
--- Flight/decision.js
+++ Flight/decision.js
@@ -5,3 +5,10 @@ exports.data = function(client) {
 	client.config('general:navdata_demo', 'TRUE');
 }
 
+exports.navdata = function navdata(client) {
+	client.on('navdata', console.log);
+}
+
+exports.lightShow = function lightShow(client) {
+	client.animateLeds('blinkOrange', 3, 15);
+}
