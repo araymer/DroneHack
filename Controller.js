cd1493ed26d8b79d611bed74cf7f52ba5677123b
Ethan <emanns95@gmail.com>
2016-01-24 01:05:17 -0700

    Removed low battery and return home and added patrol function. Added test.js Switched to autonomous drone stuff for rotation

--- Controller.js
+++ Controller.js
@@ -7,40 +7,6 @@ var motion = require('./Flight/motion');
 
 var data;
 
-/*
-Test of movements using ar-drone api
-
-motion.lightShow(client);
-client.takeoff();
-client.after(5000, function() {
-	this.up(1);
-}).after (750, function() {
-	this.front(.07);
-}).after(6000, function() {
-	this.stop();
-}).after(3000, function() {
-	this.clockwise(.05);
-}).after(9000, function() {
-	this.stop();
-}).after(3000, function() {
-	this.land();
-}).after(3000, function() {
-	process.exit();
-});*/
-
-/* Test of Motion commands interrupting patrol
-*/
-
-// client.takeoff();
-// 	client.after(3000, function () {
-// 		this.front(1);
-// 	}).after(3000, function () {
-// 		motion.avoidLeft(client);
-// 	}).after(10000, function () {
-// 		this.land();
-//		process.exit();
-// 	});
-
 motion.patrol(client);
 //TODO: listen for interruptions from the IR sensors
 
--- Flight/motion.js
+++ Flight/motion.js
@@ -1,32 +1,18 @@
 
 var exports = module.exports = {};
 
-var state = "STANDBY";
-/*States:
-STANDBY, HOVER, FORWARD, BACK, LEFTTURN, RIGHTTURN, LEFT, RIGHT, UP, DOWN,
-LANDING, TAKEOFF
-*/
-
-//Get drone state and print it to console
-exports.data = function(client) {
-	client.on('navdata', console.log);
-};
-
-//Flashes LEDs orange, used for testing
-exports.lightShow = function (client) {
-	client.animateLeds('blinkOrange', 3, 5);
-};
-
 /**************************************************
-ADVANCED FUNCTIONS
+MOTION FUNCTIONS
 	These functions all return the client to allow
 	chaining of functions in a builder pattern.
-	These are meant to use to cut down on repetitive
-	code when handling IR events.
+	These functions are meant to dictate which motions
+	that should be performed under different
+	circumstances.
 ***************************************************/
-//Takeoff and landing included to return the client
-//	upon callback to allow it to fit into the builder
-//	pattern.
+/*Takeoff and landing included to return the client
+ *	upon callback to allow it to fit into the builder
+ *	pattern.
+ */
 exports.takeoff = function (client) {
 	var takeoffCallback = function() {return client;};
 	client.takeoff(takeoffCallback);
@@ -37,44 +23,53 @@ exports.land = function (client) {
 	client.land(landCallback);
 };
 
+//Begins patrol (moving forward)
+exports.patrol = function (client) {
+	client.front(1);
+}
+
+/*
+ * Should be performed when the right IR sensor
+ * OR BOTH SENSORS detect an incoming obstacle.
+ */
 exports.avoidLeft = function (client) {
 	client.stop();
-	state = "LEFTTURN";
 	client.after(2000, function() {
-		this.back(.2);
-	}).after(2000, function() {
-		this.stop();
-	}).after(2000, function() {
 		this.counterClockwise(.2);
 	}).after(3000, function() {
 		this.stop();
 	}).after(2000, function() {
-		exports.patrol(client);
+		this.front(1);
+	}).after(2000, function() {
+		this.stop();
+	}).after(2000, function() {
+		//this.clockwise(.2);
+		mission.ccw(90).run();
+	}).after(3000, function() {
+		this.front(1);
 		return client;
 	});
 };
 
+/*
+ * Should be performed when the left IR sensor
+ * detects an incoming obstacle.
+ */
 exports.avoidRight = function (client) {
 	client.stop();
-	state = "RIGHTTURN";
 	client.after(2000, function() {
-		this.back(.2);
-	}).after(2000, function() {
-		this.stop();
-	}).after(2000, function() {
 		this.clockwise(.2);
 	}).after(3000, function() {
 		this.stop();
 	}).after(2000, function() {
-		exports.patrol(client);
+		this.front(1);
+	}).after(2000, function() {
+		this.stop();
+	}).after(2000, function() {
+		//this.clockwise(.2);
+		mission.cw(90).run();
+	}).after(3000, function() {
+		this.front(1);
 		return client;
 	});
-};
-
-exports.returnHome = function (client) {
-
-};
-
-exports.lowBattery = function (client) {
-	exports.returnHome(client);
 };
