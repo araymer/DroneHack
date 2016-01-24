eea63009ab4b5b989213e937a3b47c6890578c17
araymer <aaronraymer26@gmail.com>
2016-01-24 04:06:22 -0700

    controller

--- Controller.js
+++ Controller.js
@@ -1,16 +1,31 @@
 
-
-var arDrone = require('ar-drone');
+var const cruisingHeight = 
 var motion = require('./Flight/motion');
 var track = require('./Flight/track');
 var security = require('.Security/alert');
 var client = require('client');
+var copterface = require('./Recog/copterface');
 
 var droneClient = arDrone.createClient();
 
-function sensorReaction(sensors) {
-	if(sensors.L == true) {
-		motion.avoidLeft();
-		track.
-	}
+function startUp() {
+	motion.takeoff();
+	newLeg();
 }
+
+function newLeg() {
+	track.newDest();
+	var xDist = Math.abs(track.destPos.x - track.curPos.x);
+	var yDist = Math.abs(track.destPos.y - track.curPos.y);
+	if(xDist <= yDist) {
+		if(track.destPos.x <= track.curPos.x) {
+			motion.rotate()
+		}
+	}
+
+}
+
+Object.observe(track.reachedX, function(changes) {
+	if(track.reachedX && track.reachedY)
+		newLeg();
+});
--- Flight/track.js
+++ Flight/track.js
@@ -11,10 +11,11 @@ var curPos = {x: 0, y: 0};
 var destPos = {x: 0, y: 0};
 var prevPos = {x: 0, y: 0};
 var FPS = 4;
-var miliSec = 0;
+var currMillisec = 0;
 var height = 0;
 var batteryDying = false; 
-
+var reachedX = false;
+var reachedY = false;
 
 
 exports = {
@@ -26,20 +27,17 @@ exports = {
 	prevPos: prevPos,
 	height: height,
 	batteryDying: batteryDying,
+	reachedX: reachedX,
+	reachedY: reachedY,
 
 
 	newDest: function(){
-		curPos = {x: (Math.Random() * 2 * max) - max, y: (Math.Random() * 2 * max) - max};
+		curPos = {x: Math.floor((Math.Random() * 2 * max) - max), y: Math.floor((Math.Random() * 2 * max) - max)};
+		currMillisec = 0;
 	},
 	updatePos: function(){
-		d = client.on('navdata'), function(data){
-				tmp = json.parse(data);
-				velocity = tmp.demo.velocity;
-				batteryDying = tmp.droneState.lowBattery;
-			}
-		}
 		deltaDistance = velocity * milliSec;
-		updatePos(deltaDistance);
+		updateDir(deltaDistance);
 	},
 	updateDir: function(deltaDistance){
 		if(dir === NORTH){
@@ -58,6 +56,32 @@ exports = {
 	startTime: function(){
 		miliSec += 50;
 		setTimeOut(startTime, 50);
+	},
+	goHome: function(){
+		destPos(x = 0, y = 0);
+		prevPos = curPos;
+		setHomeDir();
+	}
+	setHomeDir: function(){
+		if(0 < curPos.x && 0 < curPos.y){
+			dir = SOUTH;
+		}
+		if(0 < curPos.x && 0 > curPos.y){
+			dir = WEST;
+		}
+		if(0 > curPos.x && 0 > curPos.y){
+			dir = NORTH;
+		}
+		if(0 > curPos.x && 0 < curPos.y){
+			dir = EAST;
+		}		
 	}
 }
+
+d = client.on('navdata', function(data){
+				tmp = json.parse(data);
+				velocity = tmp.demo.velocity;
+				batteryDying = tmp.droneState.lowBattery;
+			}
+		});
 
