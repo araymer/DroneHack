5636c509b55dc1f443d4de7b1a3187c9734e3c86
Ethan <emanns95@gmail.com>
2016-01-24 01:05:17 -0700

    Removed low battery and return home and added patrol function. Added test.js

--- Controller.js
+++ Controller.js
@@ -8,7 +8,25 @@ var client = require('client');
 
 var droneClient = arDrone.createClient();
 
+var data;
 
+motion.patrol(client);
+//TODO: listen for interruptions from the IR sensors
 
+/**
+* @param client the client to the drone
+* Simple call for executing whatever the current motion is.
+*
+**/
+function executeIt() {
+	//get motion and execute.
+}
 
-
+/**
+ param client - the client to the drone.
+* @return the nav data from the drone.
+* gets and returns the nav data for the drone.
+**/
+function getData(client) {
+	return motion.data(client);
+}
